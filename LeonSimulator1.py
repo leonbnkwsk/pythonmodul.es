8fd57d2cedf8976be7aedd80e11828caf847ac1c
bieniek 2016 <leonbnkwsk@gmail.com>
2025-01-06 11:52:54 +0100

    Add files via upload

---
+++ LeonSimulator1.py
@@ -0,0 +1,7 @@
+from time import sleep
+def main():
+    1 = input("podaj rozmiar pizzy:")
+    2 = input("podaj składniki pizzy:")
+    print("rozmiar pizzy: " + 1 + " ;" + "składniki pizzy: " + 2 + ".")
+    return main()
+main()    
